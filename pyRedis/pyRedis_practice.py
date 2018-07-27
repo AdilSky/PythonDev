08438697f59251ea88924329cef199e2a06a7528
yangyaojun03946 <yangyaojun03946@hellobike.com>
2018-07-27 11:04:12 +0800

    添加 decode_responses=True 的注释

--- pyRedis/pyRedis_practice.py
+++ pyRedis/pyRedis_practice.py
@@ -28,7 +28,6 @@ pool = redis.ConnectionPool(host='localhost',port=6379,decode_responses=True)
 
 # 连接redis，加上decode_responses=True，写入的键值对中的value为str类型，不加这个参数写入的则为字节类型。
 
-
 r1 = redis.Redis(connection_pool=pool)
 
 r2 = redis.Redis(connection_pool=pool)
